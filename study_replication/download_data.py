9fda2d5eee42b98babe0769a74e1442aefa2a521
nicholas sanderson <nicholas.sanderson@ndm.ox.ac.uk>
2026-02-09 16:50:04 +0000

    added run reports for download

--- study_replication/download_data.py
+++ study_replication/download_data.py
@@ -5,11 +5,11 @@ import os
 
 def read_ENA_accession(file_path):
     df = pd.read_csv(file_path)
-    df['Run'] = df['alias'].str.split('_barcode').str[0]
-    df['barcode'] = df['alias'].str.split('_barcode').str[1]
+    df['Run'] = df['run_name']
+    #df['barcode'] = df['alias'].str.split('_barcode').str[1]
     return df
 
-# ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR158/098/ERR15836098/ERR15836098_1.fastq.gz
+# https://ftp.sra.ebi.ac.uk/vol1/run/ERR158/ERR15818380/reads.fastq.gz
 
 def download_data(ena_data):
     # makes runs folders if they don't exist
@@ -21,16 +21,18 @@ def download_data(ena_data):
     for index, row in ena_data.iterrows():
         run = row['Run']
         barcode = row['barcode']
-        accession = row['accession']
-        url = f"ftp://ftp.sra.ebi.ac.uk/vol1/fastq/{accession[:6]}/{accession[-3:]}/{accession}/{accession}.fastq.gz"
-        output_file = os.path.join('workflow_runs', run, 'data', f"barcode{barcode}.fastq.gz")
+        accession = row['run_accession']
+        # get ERS accession from the ERR accession
+        # construct the URL for the FASTQ file
+        url = f"ftp://ftp.sra.ebi.ac.uk/vol1/run/{accession[:6]}/{accession}/reads.fastq.gz"
+        output_file = os.path.join('workflow_runs', run, 'data', f"barcode{barcode:02d}.fastq.gz")
         os.makedirs(os.path.dirname(output_file), exist_ok=True)
         # download the file using wget
         os.system(f"wget -O {output_file} {url}")
 
 if __name__ == "__main__":
     # Define the path to the CSV file
-    csv_file_path = os.path.join('meta_data', 'ENA_accessions.csv')
+    csv_file_path = os.path.join('meta_data', 'ena_webin_reports.csv')
     
     # Read the ENA accession data
     ena_data = read_ENA_accession(csv_file_path)
