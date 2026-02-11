45cd973d433d3c0246d069f27a912248c0fe8bb1
nicholas sanderson <nicholas.sanderson@ndm.ox.ac.uk>
2026-02-11 09:57:58 +0000

    fixed run_all.bash script

--- study_replication/run_all.bash
+++ study_replication/run_all.bash
@@ -3,7 +3,6 @@ AD_winter_study_220125
 AD_winter_study_070325
 AD_winter_study_170325
 AD_winter_study_100425
-AD_winter_study_090525
 AD_winter_study_160725
 AD_winter_study_220725
 AD_winter_study_240725
@@ -13,11 +12,13 @@ AD_winter_study_130825_rpt050825"
 
 soft=$1
 
+baseDir=${PWD}
+
 for run in ${runs}
 do
         #mkdir -p ${run}
         cp workflow_commands.bash workflow_runs/${run}/
         cd workflow_runs/${run}
         bash workflow_commands.bash ${soft}
-        cd ../..
+        cd ${baseDir}
 done
